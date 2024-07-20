b8c439d8f1078569258d08e88770375cc1c5178b
DarthPackman <packman150@hotmail.com>
2024-07-20 01:36:56 -0700

    slight update

--- scripts/player.gd
+++ scripts/player.gd
@@ -18,9 +18,8 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.'
 	# Get input direction of -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
 	
