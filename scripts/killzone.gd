7ca142ab406295c031380814315989252017c977
DarthPackman <packman150@hotmail.com>
2024-07-20 17:29:21 -0700

    fixed killzone

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,15 +2,18 @@ extends Area2D
 
 @onready var timer = $Timer
 @export var dmg = 0
+@export var insta_death = false
 var restart = false
 
 
 func _on_body_entered(body):
-	restart = body.take_damage(dmg)
-	if restart:
-		Engine.time_scale = 0.5
+	if not insta_death:
+		restart = body.take_damage(dmg)
+		if restart:
+			Engine.time_scale = 0.5
+			timer.start()
+	else:
 		timer.start()
-
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
