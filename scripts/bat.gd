4a44a23ae1683cbc54131b0a132aea26195dd05c
Gavin Edwards <packman150@hotmail.com>
2024-07-24 16:16:58 -0700

    Update basic bat ai

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -11,14 +11,15 @@ var is_dead = false
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var raycast_right = $"Raycast Right"
 @onready var raycast_left = $"Raycast Left"
+@onready var ray_cast_down = $RayCastDown
 @onready var collision_shape_2d = $hurtbox/CollisionShape2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if raycast_right.is_colliding():
+	if raycast_right.is_colliding() || !ray_cast_down.is_colliding():
 		animated_sprite.flip_h = true
 		direction = -1
-	if raycast_left.is_colliding():
+	if raycast_left.is_colliding() || !ray_cast_down.is_colliding():
 		animated_sprite.flip_h = false
 		direction = 1
 	position.x += direction * SPEED * delta
